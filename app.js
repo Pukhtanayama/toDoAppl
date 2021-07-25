251b602dfeaa6ccb50b7dc999611dc1d4aa6f323
Pukhtanayama <86873212+Pukhtanayama@users.noreply.github.com>
2021-07-25 13:18:47 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+var a = document.getElementById("main")
+var input = document.getElementById("input")
+
+function clickBtn(){
+    var newElement = document.createElement("P")
+    var text = input.value
+    text = document.createTextNode(text)
+    newElement.appendChild(text)
+    a.appendChild(newElement)
+
+    var editBtn = document.createElement("Button")
+    var editBtnText = document.createTextNode("edit")
+    editBtn.appendChild(editBtnText)
+    editBtn.setAttribute("onclick" , "editTodo(this)")
+    newElement.appendChild(editBtn)
+
+    var deleteBtn = document.createElement("Button")
+    var deleteBtnText = document.createTextNode("delete")
+    deleteBtn.appendChild(deleteBtnText)
+    deleteBtn.setAttribute("onclick" , "deleteTodo(this)")
+    newElement.appendChild(deleteBtn)
+}
+
+function editTodo(e){
+    e.parentNode.firstChild.nodeValue = prompt()
+}
+
+function deleteTodo(e){
+    e.parentNode.remove()
+}
+
+
